4bd8c52a07e92a890396082080638b8548f9f5b3
hane smitter <hanesmitter3@gmail.com>
2020-06-20 03:58:00 +0300

    init commit

---
+++ script.js
@@ -0,0 +1,15 @@
+const signUpButton = document.getElementById('signUp');
+const signInButton = document.getElementById('signIn');
+const container = document.getElementById('container');
+
+signUpButton.onclick = () => {
+    container.classList.add('right-panel-active');
+}
+signInButton.addEventListener('click', () => {
+    container.classList.remove('right-panel-active');
+});
+
+
+console.log(container)
+console.log(signInButton)
+console.log(signUpButton)
